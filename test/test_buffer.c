d14c10f4544959266855a2db136fdbe0b551f43f
Erik Hofman <erik@ehofman.com>
2023-02-10 11:24:34 +0100

    Fix MSV builds

--- test/test_buffer.c
+++ test/test_buffer.c
@@ -7,7 +7,9 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#if HAVE_UNISTD_H
+# include <unistd.h>
+#endif
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
